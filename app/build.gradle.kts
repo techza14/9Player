637e8f679d706d901efede331eec41f18816b031
techza14 <techza14@users.noreply.github.com>
2026-03-14 04:20:35 +0800

    release: prepare 1.2.1 signed apk and release workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "moe.tekuza.m9player"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
