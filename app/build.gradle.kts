a9c801b219daacc4299542bfb024bf9e1625d574
techza14 <techza14@users.noreply.github.com>
2026-03-31 22:51:43 +0800

    release: 1.3.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "moe.tekuza.m9player"
         minSdk = 29
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.3.4"
+        versionCode = 17
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
