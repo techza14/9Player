b89ac2b55ce1e6fcfc4a5b6a5310ecba69bb062f
techza14 <techza14@users.noreply.github.com>
2026-04-21 16:45:44 +0800

    Bump version to 1.4.4 (code 28) for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "moe.tekuza.m9player"
         minSdk = 29
         targetSdk = 36
-        versionCode = 27
-        versionName = "1.4.3"
+        versionCode = 28
+        versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
