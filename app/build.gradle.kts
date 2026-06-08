20eb1badfb8c718d44277b32857d301068bc77cf
techza14 <techza14@users.noreply.github.com>
2026-06-08 20:50:09 +0800

    release: 1.6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "moe.tekuza.m9player"
         minSdk = 29
         targetSdk = 36
-        versionCode = 40
-        versionName = "1.6.9"
+        versionCode = 41
+        versionName = "1.6.10"
 
         externalNativeBuild {
             cmake {
@@ -155,6 +155,7 @@ tasks.register("buildMdictRust") {
 tasks.named("preBuild") {
     dependsOn("buildMdictRust")
 }
+
 
 
 
